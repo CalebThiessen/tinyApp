21cdff22aca1673d7bb55c009b5ce5b57e7899ef
CliffGreene <lifeguardtwenty@gmail.com>
2020-01-20 18:13:44 -0700

    added short url getter?

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8090; // default port 8080
 
 app.set("view engine", "ejs");
 
@@ -17,6 +17,12 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL,longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+  
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
